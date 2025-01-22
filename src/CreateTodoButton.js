435ceea8d3dbaee7ea91c7320dce40a35c1bba08
Hurtadx <andreshm253@gmail.com>
2025-01-22 15:53:21 -0500

    Add Font Awesome icons and enhance CreateTodoButton modal

--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import './CreateTodoButton.css';
@@ -16,15 +17,23 @@ function CreateTodoButton() {
             <button 
                 className="CreateTodoButton"
                 onClick={onClickButton}
-            >
-                <FontAwesomeIcon icon={faPlus} />
+            ><FontAwesomeIcon icon={faPlus}
+                 />
             </button>
             <Modal isOpen={isOpen} onClose={()=> setIsOpen(false)}>
                 <form>
                     <h2>Añadir Nuevo ToDO</h2>
-                    <input type="text" placeholder="Escribe"/>
+                    <input type="text" placeholder="Escribe el ToDo"/>
                 </form>
-                
+                <div className='Modal-butoonContainer'>
+                    
+                    <button onClick={()=> setIsOpen(false)}>
+                        <FontAwesomeIcon icon={faXmark}/>
+                    </button>
+                    <button type='submit' className='Modal-button Modal-buttoon-add'>
+                        <FontAwesomeIcon icon={faPlus}/>
+                    </button>
+                </div>
             </Modal>
         </>
     );
