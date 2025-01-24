aa48fc64335548e316322bd41f6e63cf15ff7b82
Hurtadx <andreshm253@gmail.com>
2025-01-24 14:58:08 -0500

    Update TodoItem background color and implement search functionality in TodoSearch component

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -4,9 +4,16 @@ import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import './CSS/TodoSearch.css';
 
 function TodoSearch() {
+    const [searchValue, setSearchValue] = React.useState('');
+
+
+
     return (
         <div className="search-container">
-            <input placeholder=" Buscar..." />
+            <input placeholder=" Buscar..." className='TodoSearch' onChange={(event)=>{
+                
+                
+            }}/>
             <FontAwesomeIcon 
                 icon={faMagnifyingGlass} 
                 className="search-icon"
